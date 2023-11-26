e44763b18a7e0e5736accc54489d4456e6473c54
Caio <caioarrudar.l2003@gmail.com>
2023-11-25 22:44:17 -0300

    Abertura feature ajuste version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='comente_sobre',
-    version='1.0.0',
+    version='feature-botao-like-assincrono-SNAPSHOT',
     description='Projeto para a disciplina de Engenharia de Software, visando a criação de um sistema de comentários.',
 )
